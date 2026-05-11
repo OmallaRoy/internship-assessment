84bb425de5495816a074e237af7e487c6b438d5b
OmallaRoy <omallaroy25@gmail.com>
2026-05-11 16:32:03 +0300

    Implement the logic for collatz function

--- exercises/basics.py
+++ exercises/basics.py
@@ -2,17 +2,32 @@ from typing import List
 
 
 def collatz(n: int) -> List[int]:
-    """
-    You're given a positive integer n. Write an algorithm that does the following:
-        - If n is even, the algorithm divides n by 2. This is the new value of n
-        - If n is odd, the algorithm multiplies it by 3 and adds 1. This is the new value of n.
-        - The algorithm repeats this until n == 1.
+       
+       # Handle case where a user enters a negative integer or decimal
+       if type(n) is not int or n < 1:
+         return "The number entered has to be a positive whole integer. "
+     
+    # Initialize the list to store various collections of the numbers
+       integers = [n]
+       
+    # While loop that repeats provided that the  integer(n)  is not equal to  1
+       while n != 1:
+           
+           # Condition(if-else) to check if the integer is odd or even.
+           if n % 2 == 0:
+               n = n // 2 # if the n is even,then divide it by 2
+           else:
+               n = n * 3 + 1 # if n is odd,then multiply by 3 and add by 1
+               
+           integers.append(n)
+       
+       return integers 
 
-    Implement this algorithm in this function and return a list of all the intermediate values of n.
-    For example, if n = 3, the sequence of values is: 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
-    So, your function would return: [3, 10, 5, 16, 8, 4, 2, 1]
-    """
-    pass
+    
+               
+          
+           
+      
 
 
 def distinct_numbers(numbers: List[int]) -> int:
